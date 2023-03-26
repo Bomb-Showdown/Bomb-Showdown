654ddccd36f850818e410b9b6cc0cfb4959d7abd
Andrés Ariza <andres-ariza70@hotmail.com>
2023-03-26 18:02:29 -0500

    Corrección

--- src/main/java/edu/eci/arsw/bombshowdownapi/BombShowdownApplication.java
+++ src/main/java/edu/eci/arsw/bombshowdownapi/BombShowdownApplication.java
@@ -1,4 +1,4 @@
-package edu.eci.arsw.bombshowdown;
+package edu.eci.arsw.bombshowdownapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
